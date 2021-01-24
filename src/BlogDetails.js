def9c0c352633c58d0539c32de2bdeb3c661b26e
Tomasz Swiatek <tomasswiatek@gmail.com>
2021-01-24 18:41:45 +0100

    deleting single blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
@@ -7,6 +7,14 @@ const BlogDetails = () => {
     // we destructure what ever param is, so we use {} in the name
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
     return (
         <div className="blog-details">
             {
@@ -22,6 +30,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p><small>Written by {blog.author}</small></p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>delete</button>
                 </article>
             }
         </div >
